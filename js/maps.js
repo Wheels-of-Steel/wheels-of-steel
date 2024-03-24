536b566a8ddf02a97c837542528c5608f5dd6282
Jesse Emery <emeryj6@mymacewan.ca>
2024-03-24 11:15:58 -0600

    added map zoom and stops

--- js/maps.js
+++ js/maps.js
@@ -27,9 +27,63 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZW1lcnlqNiIsImEiOiJjbHU0dGRkZzUxYWxwMm5wZHJ4e
                 zoom: 11,
             });
 
+            var ward_stops = fetchAsync('http://127.0.0.1:5000/stops_by_ward?ward_name=' + wardValue)
             
+            ward_stops.then(result => {
+                // do things with the result here, like call functions with them
+                map.getSource('stops').setData(result)
+                map.addLayer({
+                    'id': 'stops-layer',
+                    'type': 'circle',
+                    'source': 'stops',
+                    'paint': {
+                        'circle-color': 'rgba(255, 0, 0, 1)'
+                    }
+                });
 
+                setTimeout(function() {
+                    resultFeatures = map.queryRenderedFeatures({layers: ['stops-layer']});
+                    //console.log(resultFeatures);
+                    // TODO: loop through the resultFeatures, getting the stop number for each feature
+                    var ward_stops_ids = [];
+                    console.log(resultFeatures);
+                    for (var i in resultFeatures) {
+                        ward_stops_ids.push(resultFeatures[i].properties.stop_id)
+                      }
+                    //Loop through the location names
+                    ward_stops_ids.forEach(function(stop_id) {
+                        // Create a checkbox element
+                        var checkbox = document.createElement('input');
+                        checkbox.type = 'checkbox';
+                        checkbox.value = stop_id;
+                        checkbox.id = 'stop' + stop_id; // Unique ID for each checkbox
+
+                        // Create a label for the checkbox
+                        var label = document.createElement('label');
+                        label.htmlFor = 'stop' + stop_id;
+                        label.appendChild(document.createTextNode('Stop Number ' + stop_id));
+
+                        // Create a div for the form-check class
+                        var formCheckDiv = document.createElement('div');
+                        formCheckDiv.classList.add('form-check');
+
+                        // Append the checkbox and label to the div
+                        formCheckDiv.appendChild(checkbox);
+                        formCheckDiv.appendChild(label);
+
+                        // Append the div to the container
+                        container.appendChild(formCheckDiv);
+                    });
+                }, 500);
+            })
         }
+
+        async function fetchAsync (url) {
+            let response = await fetch(url);
+            let data = await response.json();
+            return data;
+        }
+
         function filterMap(layer_name, property_name, values) {
             map.setFilter(layer_name, ['match', ['get', property_name], values, true, false]);
         }
@@ -84,14 +138,14 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZW1lcnlqNiIsImEiOiJjbHU0dGRkZzUxYWxwMm5wZHJ4e
                 }
             });
 
-            map.addLayer({
-                'id': 'stops-layer',
-                'type': 'circle',
-                'source': 'stops',
-                'paint': {
-                    'circle-color': 'rgba(255, 0, 0, 1)'
-                }
-            });
+            // map.addLayer({
+            //     'id': 'stops-layer',
+            //     'type': 'circle',
+            //     'source': 'stops',
+            //     'paint': {
+            //         'circle-color': 'rgba(255, 0, 0, 1)'
+            //     }
+            // });
             
             let layer_name = 'wards-layer';
             let property_name = 'name_2';
@@ -101,7 +155,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZW1lcnlqNiIsImEiOiJjbHU0dGRkZzUxYWxwMm5wZHJ4e
             filterMap(layer_name, property_name, values);
             filterMap('wards-layer-outline', property_name, values);
             filterMap('routes-layer', 'route_id', ['']);
-            filterMap('stops-layer', 'stop_id', ['']);
+            //filterMap('stops-layer', 'stop_id', ['']);
             // const style = map.getStyle();
             // style.layers.find(({ id }) => id === "wards-layer").paint['fill-color']['type'] = 'identity';
             // style.layers.find(({ id }) => id === "wards-layer").paint['fill-color']['property'] = 'rgb(0, 255, 0)';
