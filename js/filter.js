7b837a88174c4422b9374b1de0f6eb9799fa036f
Brant-Harker <brant.harker12@gmail.com>
2024-03-24 07:46:29 -0600

    add routes by stop to filter

--- js/filter.js
+++ js/filter.js
@@ -23,3 +23,31 @@ function getStopsByRoutes(routes) {
                console.error("Unable to fetch data:", error));
         
     }
+
+function getRoutesByStops(stops) {
+    return fetch("backend/data_sets/ETS_routes_stops.json")
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error
+                    (`HTTP error! Status: ${res.status}`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data[0]);
+
+            let routeArray = [];
+            for (let i = 0; i < data.length; i++ )  {
+                if ( stops.includes(data[i].stop_id) ) {
+                    if ( !routeArray.includes(data[i].route_id) ) {
+                        routeArray.push(data[i].route_id);
+                    }
+                }
+            }
+            console.log(routeArray);
+            return routeArray;
+        })
+        .catch((error) => 
+            console.error("Unable to fetch data:", error));
+
+}
